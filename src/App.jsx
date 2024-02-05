395bea351e9541211c76274c2fe4935750371c63
Sai Tharun <128915167+saithaRun-max@users.noreply.github.com>
2024-02-05 14:41:38 +0530

    day 1

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+import GeneratePassword from './GeneratePassword'
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <GeneratePassword />
+       
+    </>
+  )
+}
+
+export default App
